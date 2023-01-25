2af63a6bdf63eaaf26b2a97de78b1abec3a1fb43
H-JungJaeHeon <jjhking222@naver.com>
2023-01-26 03:31:03 +0900

    전투 캐릭터 이동 및 충돌 구현 완료, 전투 캐릭터 점프 구현중(30%)
    전투 캐릭터 이동 및 충돌 구현 완료, 전투 캐릭터 점프 구현중(30%)

--- Assets/Scripts/BattleScripts/BasicCharacter.cs
+++ Assets/Scripts/BattleScripts/BasicCharacter.cs
@@ -20,9 +20,25 @@ public class BasicCharacter : MonoBehaviour
     protected float speed;
     #endregion
 
-    //[SerializeField]
-    //[Tooltip("자신의 Rigidbody2D 컴포넌트")]
-    //Rigidbody2D rigid;
+    #region 대쉬 관련 모음
+    [Header("대쉬 관련 모음")]
+
+    [Tooltip("대쉬 파워(속력)")]
+    private const int DASH_POWER = 1000;
+
+    [Tooltip("대쉬 쿨타임인지 판별")]
+    private bool isDashCoolTime;
+
+    [Tooltip("대쉬 효과 종료 딜레이")]
+    WaitForSeconds dashDelay = new WaitForSeconds(0.1f);
+
+    [Tooltip("대쉬 쿨타임 딜레이")]
+    WaitForSeconds dashCoolTime = new WaitForSeconds(2f);
+    #endregion
+
+    [SerializeField]
+    [Tooltip("자신의 Rigidbody2D 컴포넌트")]
+    Rigidbody2D rigid;
 
     Vector3 speedVector;
 
@@ -33,7 +49,7 @@ public class BasicCharacter : MonoBehaviour
         StartSetting();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
@@ -41,22 +57,84 @@ public class BasicCharacter : MonoBehaviour
     void StartSetting()
     {
         speedVector = new Vector3(speed, 0, 0);
+        StartCoroutine(Dash());
     }
 
+    IEnumerator Jump()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rigid.AddForce(Vector2.up * 10);
+            }
+
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// 대쉬 키 입력 받고 대쉬 시작하는 함수
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Dash()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && isDashCoolTime == false)
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    rigid.AddForce(Vector2.left * DASH_POWER);
+                }
+                else
+                {
+                    rigid.AddForce(Vector2.right * DASH_POWER);
+                }
+
+                isDashCoolTime = true;
+
+                StartCoroutine(DashEffect());
+            }
+
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// 대쉬 쿨타임 및 대쉬 효과 종료 함수
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator DashEffect()
+    {
+        yield return dashDelay;
+
+        rigid.velocity = Vector2.zero;
+
+        yield return dashCoolTime;
+
+        isDashCoolTime = false;
+    }
+
+    /// <summary>
+    /// 이동 함수
+    /// </summary>
     private void Move()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position -= speedVector * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.position += speedVector * Time.deltaTime;
         }
-
-        //rigid.velocity = Vector3.zero;
     }
 
+    /// <summary>
+    /// 공격 맞았을 때 실행하는 함수
+    /// </summary>
+    /// <param name="damage"> 현재 받은 데미지 </param>
     public void Hit(int damage)
     {
         hp -= damage;
@@ -65,6 +143,10 @@ public class BasicCharacter : MonoBehaviour
         StartCoroutine(nowHitAction);
     }
 
+    /// <summary>
+    /// 공격 맞았을 때 캐릭터 효과 함수
+    /// </summary>
+    /// <returns></returns>
     IEnumerator HitAction()
     {
 
